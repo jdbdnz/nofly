aa521c41f12022b052bb1b16473ab35abcdd975a
Josh Dean <jdbdean@gmail.com>
2015-05-10 14:08:19 +1200

    communications

--- lib/nofly.rb
+++ lib/nofly.rb
@@ -1,6 +1,4 @@
 require 'nofly/event_library'
-require 'nofly/foo'
-require 'nofly/foo_nofly'
 
 module Nofly
 
@@ -8,7 +6,7 @@ module Nofly
     begin
       Nofly.const_get("#{obj.class.to_s}Nofly").new(obj, options)
     rescue
-      raise "Cannot nofly that. Have you forgot to create a nofly event library for '#{obj.class.to_s}?'"
+      raise "Cannot nofly that. Have you forgotten to create a nofly event library for '#{obj.class.to_s}?'"
     end
   end
 
